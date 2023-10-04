6f5f58787de58ec2d59b87a7d697a2c001e68454
FixedPoint Experimental <141958903+fixedpointexperimental@users.noreply.github.com>
2023-10-04 12:10:13 +0100

    Update artemis_1_0_5.py

--- artemis_1_0_5.py
+++ artemis_1_0_5.py
@@ -774,44 +774,4 @@ zip_filename = f"artemis_{current_datetime}.zip"
 from google.colab import files
 files.download(f"/content/{zip_filename}")
 
-"""# License  and Copyright
 
-ARTEMIS Software License Agreement
-
-**THIS SOFTWARE LICENSE AGREEMENT** ("Agreement") is entered into between **FixedPoint IO Ltd**, a company registered in England with company number 13288661 and its registered office address at 20-22 Wenlock Road, London, England, N1 7GU ("Licensor") and **Climate Impact Partners** ("Licensee").
-
-## 1. Grant of License
-Licensor hereby grants to Licensee, and Licensee accepts, a non-exclusive, non-transferable license to use the ARTEMIS software ("Software") solely for internal purposes.
-
-## 2. Ownership
-The Software is owned by Licensor and is protected by copyright law. No title to or ownership of the Software is transferred to Licensee. Licensee acknowledges and agrees that the Software and all copies thereof are owned by Licensor, and all rights, title, and interest therein shall remain with Licensor.
-
-## 3. Restrictions
-Licensee shall not:
-- Make copies of the Software or documentation.
-- Sell, lease, license, or sublicense the Software.
-- Modify, adapt, merge, or create derivative works based on the Software.
-- Reverse engineer, decompile, or disassemble the Software.
-
-## 4. Termination
-This Agreement is effective until terminated. Licensor may terminate this Agreement if Licensee breaches any of its terms and conditions. Upon termination, Licensee shall destroy all copies of the Software.
-
-## 5. Disclaimer
-The Software is provided "as is" without warranty of any kind. Licensor disclaims all warranties, either express or implied, including but not limited to, implied warranties of merchantability, fitness for a particular purpose, and non-infringement.
-
-## 6. Limitation of Liability
-Licensor shall not be liable for any damages, including consequential, incidental, direct, indirect, special, punitive, or other damages arising out of the use or inability to use the Software.
-
-## 7. Governing Law
-This Agreement shall be governed by the laws of England and Wales.
-
-## 8. Entire Agreement
-This Agreement contains the entire agreement between Licensor and Licensee and supersedes all prior negotiations, understandings, and agreements between the parties.
-
----
-
-**COPYRIGHT NOTICE:**  
-This code was developed by FixedPoint IO Ltd for Climate Impact Partners and remains the intellectual property of FixedPoint IO Ltd. Redistribution or sale, in whole or in part, is strictly prohibited without the express written consent of FixedPoint IO Ltd. By using this code, you accept all responsibilities for its use and will not hold FixedPoint IO Ltd, its employees or officers, liable for damages of any sort. Use of this code for illegal purposes is strictly prohibited.  
-For technical inquiries, contact: charles@fixedpoint.io  
-(c) 2023 FixedPoint IO Ltd.
-"""
